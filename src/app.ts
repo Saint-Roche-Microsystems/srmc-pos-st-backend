447b41738c60c27543ad7f8563d232737de9ee93
vieerr <olivier_paspuel@hotmail.com>
2026-02-12 07:41:38 -0500

    feat: 🎸 add healthcheck endpoint

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (_req, res) => {
+	res.status(200).json({ status: 'ok' });
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
